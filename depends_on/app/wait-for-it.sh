84ab3dddbefcb091ac4c67399562f5766652fae9
kotaroooo0 <k33asby@gmail.com>
2020-07-25 13:27:28 +0900

    update shell script

--- depends_on/app/wait-for-it.sh
+++ depends_on/app/wait-for-it.sh
@@ -6,8 +6,6 @@ until nc -z redis 6379; do
   >&2 echo "redis is unavailable - sleeping"
   sleep 3
 done
->&2 echo "redis is up - executing command"
-
 until nc -z mysql 3306; do
   >&2 echo "mysql is unavailable - sleeping"
   sleep 3
