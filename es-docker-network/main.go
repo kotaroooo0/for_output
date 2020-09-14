3041bf03b617f76d0c160acede77f3151331fe96
Kotaro Adachi <k33asby@gmail.com>
2020-09-14 23:49:32 +0900

    a

--- es-docker-network/main.go
+++ es-docker-network/main.go
@@ -1,32 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httputil"
+	"log"
 
 	"github.com/olivere/elastic"
 )
 
 func main() {
 	// fmt.Println("---------- elastic.NewClient() ----------")
-	// _, err := elastic.NewClient()
+	_, err := elastic.NewClient(elastic.SetURL("http://127.0.0.1:9200"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	// fmt.Println("---------- elastic.NewClient(elastic.SetSniff(false)) ----------")
+	// _, err := elastic.NewClient(elastic.SetSniff(false))
 	// if err != nil {
-	// 	fmt.Println(err)
+	// 	log.Println(err)
 	// }
 
-	fmt.Println("---------- elastic.NewClient(elastic.SetSniff(false)) ----------")
-	_, err := elastic.NewClient(elastic.SetSniff(false))
-	if err != nil {
-		fmt.Println(err)
-	}
+	// fmt.Println(`---------- http.Get("http://127.0.0.1:9200") ----------`)
 
-	fmt.Println(`---------- http.Get("http://127.0.0.1:9200") ----------`)
-
-	res, err := http.Get("http://127.0.0.1:9200/_nodes/http?pretty=true")
-	if err != nil {
-		fmt.Println(err)
-	}
-	dr, _ := httputil.DumpResponse(res, true)
-	fmt.Println(string(dr))
+	// res, err := http.Get("http://127.0.0.1:9200/?pretty=true")
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// dr, _ := httputil.DumpResponse(res, true)
+	// fmt.Println(string(dr))
 }
