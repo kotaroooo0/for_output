fe262d6e24c0f7865ede4255a519e7942ed11e25
Kotaro Adachi <k33asby@gmail.com>
2020-09-14 20:59:02 +0900

    a

--- map_order/main.go
+++ map_order/main.go
@@ -1,16 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/golang/go/src/strings"
+)
 
 func main() {
-	m := map[string]int{
-		"a": 1,
-		"b": 2,
-		"c": 3,
-		"d": 4,
-	}
-
+	m := map[string]int{"a": 1, "b": 2, "c": 3, "d": 4}
 	for k, v := range m {
 		fmt.Print(k, v, " ")
 	}
+	strings.Join()
 }
