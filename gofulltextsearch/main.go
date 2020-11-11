52cca11264c9be549410f7fe1d873ccf15f27ba3
Kotaro Adachi <k33asby@gmail.com>
2020-11-11 21:52:37 +0900

    wip

--- gofulltextsearch/main.go
+++ gofulltextsearch/main.go
@@ -1,61 +1,64 @@
 package main
 
 import (
-	"github.com/k0kubun/pp"
+	"fmt"
+
 	"github.com/kotaroooo0/stalefish"
 )
 
 func main() {
+	// documents := []stalefish.Document{
+	// 	{
+	// 		ID: 0,
+	// 		Fields: map[string]string{
+	// 			"name":    "Ichiro Suzuki",
+	// 			"content": "I have a lot of TASKs. I am very sad :(",
+	// 		},
+	// 	},
+	// 	{
+	// 		ID: 1,
+	// 		Fields: map[string]string{
+	// 			"name":    "Taro Tanaka",
+	// 			"content": "I have no tasks. I am HAPPY :)",
+	// 		},
+	// 	},
+	// }
+	analyzer := stalefish.Analyzer{
+		[]stalefish.CharFilter{stalefish.MappingCharFilter{map[string]string{":)": "_happy_", ":(": "_sad_"}}},
+		stalefish.StandardTokenizer{},
+		[]stalefish.TokenFilter{stalefish.LowercaseFilter{}, stalefish.StopWordFilter{}, stalefish.StemmerFilter{}},
+	}
+
 	documents := []stalefish.Document{
 		{
 			ID: 0,
 			Fields: map[string]string{
-				"first_name": "tanaka",
-				"last_name":  "taro",
-				"content":    "fine fax fox fun",
+				"content": "fine fax fox fun :)",
 			},
 		},
 		{
 			ID: 1,
 			Fields: map[string]string{
-				"first_name": "tanaka",
-				"last_name":  "ziro",
-				"content":    "fine fax fox hot",
+				"content": "fine fax fox hot :)",
 			},
 		},
 		{
 			ID: 2,
 			Fields: map[string]string{
-				"first_name": "sato",
-				"last_name":  "taro",
-				"content":    "fine fax hot pie",
-			},
-		},
-
-		{
-			ID: 3,
-			Fields: map[string]string{
-				"first_name": "sato",
-				"last_name":  "ichiro",
-				"content":    "fine best snow",
+				"content": "fine fax hot pie :)",
 			},
 		},
 	}
-	analyzer := stalefish.Analyzer{
-		stalefish.StandardTokenizer{},
-		[]stalefish.CharFilter{},
-		[]stalefish.Filter{stalefish.LowercaseFilter{}},
-	}
 
+	// Indexing
 	index := stalefish.Index{}
 	index.Indexing(documents, analyzer)
-	pp.Println(index)
 
+	// Search
 	searcher := stalefish.Searcher{index, analyzer}
 	ids := searcher.Search(stalefish.Query{
-		"fine fax fox",
+		"fine FaX foxes happy",
 		[]string{"content"},
 	})
-	pp.Println(ids)
-
+	fmt.Println(ids)
 }
